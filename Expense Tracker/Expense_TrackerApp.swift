99a45a2505db669010b1ba9ca037cb8c8ff716ef
erikson <61755589+EriksonLoures@users.noreply.github.com>
2023-12-18 19:39:59 -0300

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Erik Loures on 18/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
